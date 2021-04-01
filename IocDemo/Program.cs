701440fd4acc5442b64a7ed81f46fbd02a0e2d52
sam <sam@DESKTOP-AE3QHOG>
2021-04-01 14:24:24 +0800

    full

--- IocDemo/Program.cs
+++ IocDemo/Program.cs
@@ -26,4 +26,20 @@ namespace IocDemo
             starup.Invoke(app, parameters.ToArray());
         }
     }
+
+    class App
+    {
+        public void ConfigureMyServices(IMyServiceCollection services)
+        {
+            services.AddScoped<IMain, Main>();
+            services.AddScoped<IThreadA, ThreadA>();
+            services.AddScoped<IThreadB, ThreadB>();
+            services.AddSingleton<IThreadC, ThreadC>();
+        }
+
+        public void Starup(IMain main)
+        {
+            main.Run();
+        }
+    }
 }
